fc1c6e92de1292c98f326eb5567af846a366d266
cutesd <kimberly@kimberlycooper.me>
2018-08-16 12:51:03 -0700

    fixed scroll to top prevent

--- src/components/Col/Col.js
+++ src/components/Col/Col.js
@@ -3,9 +3,9 @@ import "./Col.css";
 
 const Col = props =>
     <div className="col-3 col-spc">
-        <a onClick={props.btnClick}>
-            <img className="img-fluid" id={props.id} src={props.img} alt={props.name} />
-        </a>
+      
+            <img className="img-fluid" id={props.id} src={props.img} alt={props.name} onClick={props.btnClick}/>
+        
     </div>;
 
 export default Col;
